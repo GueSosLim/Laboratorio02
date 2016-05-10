dcf4b887b76e45c7ad53e534452ce2cf7f95004f
rivete96 <rivete96@gmail.com>
2016-05-09 21:31:36 -0500

    cambio 4.1 v1

---
+++ Laboratorio02/4.1/Script02.sql
@@ -0,0 +1,11 @@
+﻿
+/*Creando Tabla Mis_Empleados*/
+CREATE TABLE Mis_Empleados
+(
+ID_Emp int NOT NULL,
+Apellidos varchar(25) ,
+Nombres varchar(25),
+Codigo varchar(10),
+Salario decimal(9,2),
+PRIMARY KEY (ID_Emp)
+)
---
+++ Laboratorio02/4.1/Script03.sql
@@ -0,0 +1,8 @@
+﻿
+/*Insercion de Datos*/
+
+INSERT INTO  "Mis_Empleados" ("Apellidos", "Nombres","Codigo","Salario")
+VALUES ("Vargas Canseco","Raul","Rvargas",895.0 );
+
+INSERT INTO  "Mis_Empleados" ("Apellidos", "Nombres","Codigo","Salario")
+VALUES ("Castro Feria","Maria","Mcastro",860.0 );
