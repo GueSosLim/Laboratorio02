945e0a1393a88175a2a68bde5e42639e4825b738
rivete96 <rivete96@gmail.com>
2016-05-09 21:04:16 -0500

    creado

---
+++ Laboratorio02/Script01.sql
@@ -0,0 +1,13 @@
+﻿/*
+No cambie las variables de nombre o ruta de acceso de la base de datos.
+Cualquier variable sqlcmd será correctamente sustituida durante 
+la compilación y la implementación.
+*/
+ALTER DATABASE [$(DatabaseName)]
+ADD LOG FILE
+(
+	NAME = [Script01_log],
+	FILENAME = '$(DefaultLogPath)$(DefaultFilePrefix)_Script01.ldf',
+	SIZE = 1024 KB,
+	FILEGROWTH = 10%
+)´{oo
